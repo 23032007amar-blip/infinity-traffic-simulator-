172d1f940fd7fa951fdd257885010a071cf2e46a
23032007amar-blip <23032007amar@gmail.com>
2026-07-05 16:41:10 +0530

    Initialize Garage object with car data
    Defines a Garage object with car details including id, name, price, speed, handling, image, and unlock status.

---
+++ garage.js
@@ -0,0 +1,50 @@
+// js/garage.js
+
+const Garage = {
+
+    selectedCar: 0,
+
+    cars: [
+        {
+            id: 0,
+            name: "Starter",
+            price: 0,
+            speed: 6,
+            handling: 6,
+            image: "assets/cars/player.png",
+            unlocked: true
+        },
+        {
+            id: 1,
+            name: "Sedan",
+            price: 500,
+            speed: 7,
+            handling: 7,
+            image: "assets/cars/sedan.png",
+            unlocked: false
+        },
+        {
+            id: 2,
+            name: "SUV",
+            price: 1000,
+            speed: 8,
+            handling: 6,
+            image: "assets/cars/suv.png",
+            unlocked: false
+        },
+        {
+            id: 3,
+            name: "Sports",
+            price: 2000,
+            speed: 10,
+            handling: 9,
+            image: "assets/cars/sports.png",
+            unlocked: false
+        },
+        {
+            id: 4,
+            name: "Police",
+            price: 3500,
+            speed: 11,
+            handling: 8,
+            image: "
